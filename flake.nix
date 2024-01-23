02d39152231f099fa576b6782950bd85013098e6
ShyProton <shayanr1001@gmail.com>
2024-01-23 08:25:34 -0500

    fix: adds shellHook to flake to make font size work correctly

--- flake.nix
+++ flake.nix
@@ -51,6 +51,11 @@
         boost
         crow
       ];
+
+      shellHook = with pkgs; ''
+        export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS;
+        export GIO_MODULE_DIR="${glib-networking}/lib/gio/modules/";
+      '';
     };
   };
 }
